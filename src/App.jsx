1bbbd779309825ec915ba84e5b25560748110311
Humayra-Amin <humaema4576@gmail.com>
2024-07-15 01:15:49 +0600

    app.jsx updated with container

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-// import React, { useState } from 'react';
-// import { Container, Grid } from '@mui/material';
-// import LeftNavbar from './components/LeftNavbar/LeftNavbar';
-// import RightNavbar from './components/RightNavbar/RightNavbar';
-// import { useMediaQuery, useTheme } from '@mui/material';
+import React, { useState } from 'react';
+import { Container, Grid } from '@mui/material';
+import LeftNavbar from './components/LeftNavbar/LeftNavbar';
+import RightNavbar from './components/RightNavbar/RightNavbar';
+import { useMediaQuery, useTheme } from '@mui/material';
 
-// const App = () => {
-//   const [selectedChat, setSelectedChat] = useState(null);
-//   const theme = useTheme();
-//   const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+const App = () => {
+  const [selectedChat, setSelectedChat] = useState(null);
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
-//   const handleSelectChat = (chat) => {
-//     setSelectedChat(chat);
-//   };
+  const handleSelectChat = (chat) => {
+    setSelectedChat(chat);
+  };
 
-//   const handleBack = () => {
-//     setSelectedChat(null);
-//   };
+  const handleBack = () => {
+    setSelectedChat(null);
+  };
 
-//   return (
-//     <Container maxWidth="xl">
-//       <Grid container spacing={1}>
-//         <Grid item xs={12} lg={3}>
-//           {(!isSmallScreen || !selectedChat) && (
-//             <LeftNavbar onSelectChat={handleSelectChat} />
-//           )}
-//         </Grid>
-//         <Grid item xs={12} lg={9}>
-//           {(!isSmallScreen || selectedChat) && (
-//             <RightNavbar selectedChat={selectedChat} onBack={handleBack} />
-//           )}
-//         </Grid>
-//       </Grid>
-//     </Container>
-//   );
-// };
+  return (
+    <Container maxWidth="xl">
+      <Grid container spacing={1}>
+        <Grid item xs={12} lg={3}>
+          {(!isSmallScreen || !selectedChat) && (
+            <LeftNavbar onSelectChat={handleSelectChat} />
+          )}
+        </Grid>
+        <Grid item xs={12} lg={9}>
+          {(!isSmallScreen || selectedChat) && (
+            <RightNavbar selectedChat={selectedChat} onBack={handleBack} />
+          )}
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
-// export default App;
+export default App;
